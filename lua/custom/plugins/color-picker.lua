ee191cc7e6af96628286acc54df7d0f18981771e
Chloroxite <zubersonzak@hotmail.com>
2025-01-02 12:08:35 -0600

    adding my changes

---
+++ lua/custom/plugins/color-picker.lua
@@ -0,0 +1,12 @@
+return {
+  -- add symbols-outline
+  {
+    'ziontee113/color-picker.nvim',
+    cmd = 'Cpick',
+    opts = {
+      -- add your options that should be passed to the setup() function here
+      noremap = true,
+      silent = true,
+    },
+  },
+}
